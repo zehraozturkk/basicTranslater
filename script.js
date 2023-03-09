29e0e39fb5ed07d1513980d69e00632c49d3e6e6
U200709016 <fzehraozturk34@gmail.com>
2023-03-09 21:17:06 +0300

    final settings

--- script.js
+++ script.js
@@ -5,6 +5,45 @@ const btnTranslate = document.querySelector('.btnTranslate');
 const fromtext = document.querySelector('#from-text');
 const totext = document.querySelector('#to-text');
 
+const icons = document.querySelectorAll('.icons');
+
+document.querySelector(".exchange").addEventListener('click', ()=>{
+    let text = fromtext.value;
+    fromtext.value = totext.value;
+    totext.value = text;
+    
+    let lang = fromLang.value;
+    fromLang.value = toLang.value;
+    toLang.value = lang;
+});
+
+for(let icon of icons){
+    icon.addEventListener('click', (element)=>{//buraya element diyerek hangi icona tıkladığımı alırız target özelliği sayesinde
+        if(element.target.classList.contains("fa-copy")){
+            if(element.target.id == "from"){
+                navigator.clipboard.writeText(fromtext.value); // bu  şekilde hafızaya direkt kopyalayabilriz
+            }else{
+                navigator.clipboard.writeText(totext.value);   
+            }
+        }else {
+            let uttarance;//(ifade)
+            
+            if(element.target.id == "from"){
+                uttarance = new SpeechSynthesisUtterance(fromtext.value); //nereden okiyacağını belirledik
+                uttarance.lang = fromLang.value; // hangi dilden okiyacağını belirledik
+            }else{
+                uttarance = new SpeechSynthesisUtterance(totext.value);
+                uttarance.lang = toLang.value;
+            }
+
+            speechSynthesis.speak(uttarance);
+        }
+
+        
+    })
+}
+
+
 for(let lang in languages){
 
     let option = `<option value = "${lang}">${languages[lang]}</option> `;
